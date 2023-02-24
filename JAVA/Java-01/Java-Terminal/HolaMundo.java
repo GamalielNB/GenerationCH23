55d946dcc471d5642cbc15db62c397379cddd0ab
GamalielNB <gamalielnaranjo@gmail.com>
2023-02-23 19:32:19 -0600

    Se agrega primera practiva de JAVA

---
+++ JAVA/Java-01/Java-Terminal/HolaMundo.java
@@ -0,0 +1,13 @@
+import Saludo;
+public class HolaMundo {
+
+	public static void main(String[] args) {		
+		//System.out.println("Hola Mundo desde Java");
+
+		Saludo objSaludo;
+		objSaludo = new Saludo();
+		objSaludo.saludar();
+	}
+
+}
+
